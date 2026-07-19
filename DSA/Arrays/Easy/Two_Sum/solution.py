8d221e76130b51cb8a5b0e0f890299090945b627
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-07-20 01:09:02 +0530

    Added Two Sum - Arrays - Easy

--- DSA/Arrays/Easy/Two_Sum/solution.py
+++ DSA/Arrays/Easy/Two_Sum/solution.py
@@ -1,1 +1,14 @@
-2 <= nums.length <= 104
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lst=[]
+        for i in range(len(nums)):
+            for j in range(len(nums)-1):
+
+                if  i !=j and nums[i] +nums[j] == target:
+                    lst.extend([i,j])
+                    return lst
